8ec88791c0ece2ae331b6b1928cecfbd3bdfbe69
Tej ⛤ <165182589+tejvz@users.noreply.github.com>
2025-08-25 23:10:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
-
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
+
+app.use(express.static(__dirname));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
